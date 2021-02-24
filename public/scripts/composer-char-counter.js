eb477797a0311521cc8605457c18ecdb50313172
Adam Mohammed <13.adamm@gmail.com>
2021-02-23 20:03:25 -0500

    updated tweets container to hold existing tweets; updated CSS for container

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  $(".tweet-text").on("input", function() {
+
+  const maxCharacters = 140
+  const length = $(this).val().length;
+
+  $(".counter").text(maxCharacters - length);
+
+  if (length > maxCharacters) {
+    $(".counter").css()"color","#FF0000";
+
+  } else {
+    $(".counter").css("color","#000000");
+   }
+  })
+});
