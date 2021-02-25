bd40d7d4b534ebaa59e66579fcbf585bb66e93b1
Adam Mohammed <13.adamm@gmail.com>
2021-02-25 13:14:57 -0500

    updated tweets to dynamically add; added AJAX; updated Client JS to include XSS attack deterrant/ error messages

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
   $(".counter").text(maxCharacters - length);
 
   if (length > maxCharacters) {
-    $(".counter").css()"color","#FF0000";
+    $(".counter").css("color","#FF0000");
 
   } else {
     $(".counter").css("color","#000000");
